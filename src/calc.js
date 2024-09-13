2b40c8f55da68f06143df83d187e645f36aa9333
Nicolas Glassey <nicolas.glassey@eduvaud.ch>
2024-09-13 13:39:44 +0200

    feat: soustraction - todo

---
+++ src/calc.js
@@ -0,0 +1,24 @@
+function addition(a, b) {
+    let somme = a + b;
+    return somme;
+}
+
+function multiplication(a, b) {
+    let produit = a * b;
+    return produit;
+}
+
+function soustraction(a, b){
+    //TODO
+}
+
+function main() {
+    let x = 5;
+    let y = 10;
+    let resultatAdd = addition(x, y);
+    let resultatMult = multiplication(x, y);
+    let resultatSous = soustraction(x, y);
+    console.log(`Addition: ${resultatAdd}, Multiplication: ${resultatMult}, Soustraction: ${resultatSous}`);
+}
+
+main();
